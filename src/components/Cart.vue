70bc485be5d70345b0b707629c7c02763e4dfea8
Tempestfury11 <Swiftyblue11@gmail.com>
2022-09-06 09:52:08 +0200

    card styled

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -16,20 +16,20 @@
     aria-labelledby="offcanvasExampleLabel"
   >
     <div class="offcanvas-header">
-      <button class="btn btn-primary m-3" @click="this.$store.dispatch('clearCart')">Clear Cart</button><br>
+      <button class="btn btn-grad m-3" @click="this.$store.dispatch('clearCart')">Clear Cart</button><br>
       <button
         type="button"
-        class="btn-close"
+        class="btn-close btn-grad"
         data-bs-dismiss="offcanvas"
         aria-label="Close"
       ></button>
     </div>
-    <div class="offcanvas-body text-black">
+    <div class="offcanvas-body text-white">
       <div v-for="(products, index) in cart" :key="index">
         {{ products.title }}
         <span class="text-title text-black">R{{ products.price }}</span><br>
-        {{ products.img }}
-        <button class="btn btn-primary m-3" @click="this.$store.dispatch('deleteItem', products.cart_id)">
+        <img :src="products.img" alt="">
+        <button class="btn btn-grad m-3" @click="this.$store.dispatch('deleteItem', products.cart_id)">
           Delete item
         </button>
       </div>
@@ -54,4 +54,30 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.btn-grad {
+  background-image: linear-gradient(
+    to right,
+    #000000 0%,
+    #434343 51%,
+    #000000 100%
+  );
+  margin: 10px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  display: block;
+  text-decoration: none;
+}
+
+.btn-grad:hover {
+  background-position: right center; /* change the direction of the change here */
+  color: #fff;
+  text-decoration: none;
+}
+
+</style>
