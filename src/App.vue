c02de37ced96a46dc2b0baa4157f1e26dc2faa93
Tempestfury11 <Swiftyblue11@gmail.com>
2022-09-08 16:16:56 +0200

    cardloader removed

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
     <Navbar/>
   <router-view/>
-  <CardLoader/>
     <FooterView/>
 </template>
 
 <script>
 import FooterView from "@/components/FooterView.vue";
 import Navbar from "@/components/Navbar.vue";
-import CardLoader from "@/components/CardLoader.vue";
 export default {
-  components: { FooterView, Navbar, CardLoader, },
+  components: { FooterView, Navbar, },
 };
 </script>
 
